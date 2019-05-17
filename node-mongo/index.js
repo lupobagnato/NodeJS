0352ddff4f850d010456eb8f4baf277426482de7
CaneGrigio <braccodiweimar@yahoo.it>
2019-05-17 16:00:12 +0200

    Node MongoDB Example 1

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,9 +4,29 @@ const assert = require('assert');
 const url = 'mongodb://menhir:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client)=>{
-    assert.equal(err,null);
+MongoClient.connect(url, (err, client) => {
+    assert.equal(err, null);
     console.log('Connected correctly to server');
 
-    client.close();
+    const db=client.db(dbname);
+    const collection=db.collection("dishes");
+    collection.insertOne({"name":"Pizza","description":"Pizza desc"}, (err, result)=>{
+        assert.equal(err,null);
+
+        console.log('After Insert:\n');
+        console.log(result.ops);
+
+        collection.find({}).toArray((err,docs)=>{
+            assert.equal(err,null);
+            console.log('Found:\n');
+            console.log(docs);
+
+            db.dropCollection("dishes", (err,result)=>{
+                assert.equal(err,null);
+
+                client.close();
+            })
+        })
+    })
+    
 });
